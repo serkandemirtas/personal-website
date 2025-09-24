1e084165fff6b4a01e327dabfe4eff8e2b56e569
Serkan Demirtaş <160789175+inception7218@users.noreply.github.com>
2025-09-24 17:32:05 +0300

    Host settings.

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "personal-website-ynqc.onrender.com"]
 
 
 # Application definition
