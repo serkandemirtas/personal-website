39dd24225a6ade2f95d8ca8ffe2a62d8ed8b67ad
Serkan Demirtaş <160789175+inception7218@users.noreply.github.com>
2025-09-25 19:26:51 +0300

    Host settings.

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['djangowebsite-env.eba-p985nnpk.eu-north-1.elasticbeanstalk.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['personal-website-vo3t.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
