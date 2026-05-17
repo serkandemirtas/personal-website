94fb410fbc6d974e7b0fe3cdfb77f8cfdcb7e1da
Serkan Demirtaş <160789175+inception7218@users.noreply.github.com>
2026-05-17 20:08:34 +0300

    Fix load_dotenv to override existing env vars

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+load_dotenv(override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
